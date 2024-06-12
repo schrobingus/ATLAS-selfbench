96c2287c7975c47672394856e490786e140c2ce5
schrobingus <brent.monning.jr@gmail.com>
2024-06-12 14:02:18 -0700

    implement testing via litellm

--- selfbench.py
+++ selfbench.py
@@ -1,7 +1,11 @@
 import os, sys
+# TODO: add garbage collection, you're gonna need it
 import copy
 from typing import Any, Dict, List
 import json
+
+import litellm
+litellm.drop_params = True
 
 def define_structure(input_dir: str, output_dir: str, principles: List[str], categories: List[str]) -> List[str]:
     output_dir_path = os.path.join(os.getcwd(), output_dir)
@@ -24,7 +28,7 @@ def define_structure(input_dir: str, output_dir: str, principles: List[str], cat
                     templates_needed.append(f"principles/{category}/{principle_file}")
     return templates_needed
 
-def create_template(input_dir: str, output_dir: str, models_to_use: List[str], template_needed: str) -> None:
+def create_template(input_dir: str, output_dir: str, models_to_use: List[str], template_needed: str) -> List[Dict[str, Any]]:
     with open(f"{input_dir}/{template_needed}", "r") as file:
         template_base = json.load(file)
     model_base = "gpt4"
@@ -44,6 +48,27 @@ def create_template(input_dir: str, output_dir: str, models_to_use: List[str], t
     ]
     with open(f"{output_dir}/{template_needed}", "w") as template_dumped:
         json.dump(template, template_dumped, indent=2)
+    return template
+
+def run_tests(output_dir: str, template_id: str, template: List[Dict[str, Any]]) -> None:
+    result: List[Dict[str, Any]] = []
+    for conv_id in range(len(template)):
+        model = template[conv_id]["model"]
+        print(f"   - Identity {conv_id+1}/{len(template)} on {model}")
+        messages: List[Dict[str, str]] = []
+        for conv_message in template[conv_id]["conversations"]:
+            messages.append({"content": conv_message["value"], "role": "user"})
+            response = litellm.completion(model=model, messages=messages)
+            messages.append({"content": response.choices[0].message.content, "role": "assistant"})
+        result.append(copy.deepcopy(template[conv_id]))
+        result[conv_id]["conversations"] = []
+        for conv_message in messages:
+            result[conv_id]["conversations"].append({
+                "from": "human" if conv_message["role"] == "user" else "gpt",
+                "value": conv_message["content"]
+            })
+    with open(f"{output_dir}/{template_id}", "w") as result_dumped:
+        json.dump(result, result_dumped, indent=2)
 
 if __name__ == '__main__':
     option: str = ''
@@ -52,12 +77,14 @@ if __name__ == '__main__':
     repeats: int = 5
     principles: List[int] = list(range(1, 27))
     categories: List[str] = ['boosting', 'correctness']
-    models_to_use: List[str] = ['ollama/gemma:2b', 'gpt4', 'ollama/phi3', 'gpt-3.5-turbo']
+    models_to_use: List[str] = ['gpt4', 'gpt-3.5-turbo']
     if len(sys.argv) > 1:
         if '-p' in sys.argv:
             principles = []
         if '-c' in sys.argv:
             categories = []
+        if '-m' in sys.argv:
+            models_to_use = []
         for arg in range(1, len(sys.argv)):
             if sys.argv[arg][0] == "-":
                 if sys.argv[arg] == '-h':
@@ -80,6 +107,14 @@ if __name__ == '__main__':
                         categories.append(sys.argv[arg])
                     case '-m':
                         models_to_use.append(sys.argv[arg])
+    print("Defining output structure...")
     template_ids = define_structure(input_dir, output_dir, principles, categories)
-    for template in template_ids:
-        create_template(input_dir, output_dir, models_to_use, template)
+    templates: Dict[str, List[Dict[str, Any]]] = {}
+    print("Creating templates...")
+    for template_id in template_ids:
+        templates[template_id] = create_template(input_dir, output_dir, models_to_use, template_id)
+    print("Testing begins:")
+    for template_id, template in templates.items():
+        print(f" - {template_id}")
+        run_tests(output_dir, template_id, template)
+    print(f"Testing complete! Output is at {output_dir}")
